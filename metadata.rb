7318a42ca1a036df29c2b63e11ed00ad5b190d0f
Jan Klare <j.klare@cloudbau.de>
2017-08-17 14:19:46 +0200

    starting pike development patch
    Change-Id: I76c914f68612c0e76dcee8e0557ff62f769533db

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer       'openstack-chef'
 maintainer_email 'openstack-dev@lists.openstack.org'
 license          'Apache 2.0'
 description      'Provides the shared messaging configuration for Chef for OpenStack.'
-version          '15.0.0'
+version          '16.0.0'
 
 recipe 'server', 'Installs and configures server packages for messaging queue used by the deployment.'
 recipe 'rabbitmq-server', 'Installs and configures RabbitMQ and is called via the server recipe'
@@ -12,7 +12,7 @@ recipe 'rabbitmq-server', 'Installs and configures RabbitMQ and is called via th
   supports os
 end
 
-depends 'openstack-common', '>= 15.0.0'
+depends 'openstack-common', '>= 16.0.0'
 depends 'rabbitmq', '~> 4.10'
 
 issues_url 'https://launchpad.net/openstack-chef' if respond_to?(:issues_url)
