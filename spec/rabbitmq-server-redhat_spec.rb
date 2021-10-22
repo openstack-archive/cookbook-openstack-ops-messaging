6d907dedf3be81660bdd8d1f689c80fbf0069068
Lance Albertson <lance@osuosl.org>
2021-10-22 09:53:55 -0700

    CentOS 8 support
    - Update ChefSpec
    
    Depends-On: https://review.opendev.org/c/openstack/openstack-chef/+/814234
    Depends-On: https://review.opendev.org/c/openstack/cookbook-openstack-common/+/814235
    Change-Id: I4593ffcb9700b232a1879a08e6bbd9cdaf76c222
    Signed-off-by: Lance Albertson <lance@osuosl.org>

--- spec/rabbitmq-server-redhat_spec.rb
+++ spec/rabbitmq-server-redhat_spec.rb
@@ -1,15 +1,17 @@
 require_relative 'spec_helper'
 
 describe 'openstack-ops-messaging::rabbitmq-server' do
-  describe 'redhat' do
-    let(:runner) { ChefSpec::SoloRunner.new(REDHAT_OPTS) }
-    let(:node) { runner.node }
-    cached(:chef_run) { runner.converge(described_recipe) }
+  ALL_RHEL.each do |p|
+    context "redhat #{p[:version]}" do
+      let(:runner) { ChefSpec::SoloRunner.new(p) }
+      let(:node) { runner.node }
+      cached(:chef_run) { runner.converge(described_recipe) }
 
-    include_context 'ops_messaging_stubs'
+      include_context 'ops_messaging_stubs'
 
-    it 'does not set use_distro_version to true' do
-      expect(chef_run.node['rabbitmq']['use_distro_version']).to be_truthy
+      it 'does not set use_distro_version to true' do
+        expect(chef_run.node['rabbitmq']['use_distro_version']).to be_truthy
+      end
     end
   end
 end
